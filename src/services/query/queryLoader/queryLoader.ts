af5640ea9caf836e68ac0fb18a3716c289349044
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2023-08-04 22:26:11 +0100

    refactor: convert promise to await

--- src/services/query/queryLoader/queryLoader.ts
+++ src/services/query/queryLoader/queryLoader.ts
@@ -1,30 +1,22 @@
-import * as fs from 'fs';
+import fs from 'fs/promises';
 
 import WidgetFileType, { FileLoaderResponse } from '../../../types';
 import { messages } from '../../../messages';
 
-export default function queryLoader(widgetDir: string): Promise<FileLoaderResponse> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(
-            `${widgetDir}/${WidgetFileType.QUERY}`,
-            'utf8',
-            (error, data: string) => {
-                if (error) {
-                    if (error.code === 'ENOENT') {
-                        resolve({
-                            type: WidgetFileType.QUERY,
-                            data: '',
-                        });
-                    } else {
-                        reject(messages.invalidQuery());
-                    }
-                }
-
-                resolve({
-                    type: WidgetFileType.QUERY,
-                    data,
-                });
-            },
-        );
-    });
+export default async function queryLoader(widgetDir: string): Promise<FileLoaderResponse> {
+    try {
+        const data = await fs.readFile(`${widgetDir}/${WidgetFileType.QUERY}`, 'utf8');
+        return {
+            type: WidgetFileType.QUERY,
+            data,
+        };
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            return {
+                type: WidgetFileType.QUERY,
+                data: '',
+            };
+        }
+        throw new Error(messages.invalidQuery());
+    }
 }
